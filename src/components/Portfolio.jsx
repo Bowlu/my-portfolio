774d690e07d1bb5f3a85d9dff16c5b770bb08341
Bowlu <awoyemiboluwatife@gmail.com>
2023-07-03 10:32:54 +0100

    included logo and made page responsive

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -39,20 +39,20 @@ function Portfolio () {
             <img src={Logo} alt="logo" className="p-1"></img>
           </div>
           <div className="header">
-            <div className="pic-container absolute top-[30px] object-cover w-[100%] h-[50vh]  flex justify-center">
+            <div className="pic-container">
               <img src={Pic} alt="my profile" className="rounded-full border-[8px] border-[#d5a397]"></img>
             </div>
           </div>
         </div>
       </div>
-      <div className="body-container mt-[13rem]">
+      <div className="body-container mt-[10rem]">
         <div className="body-header">
-          <p className="body-text text-[40px] text-center text-neutral-700 font-bold">Awoyemi Boluwatife Victoria</p>
+          <p className="body-text text-center text-neutral-700 font-bold">Awoyemi Boluwatife Victoria</p>
         </div>
         <div className="flex justify-center">
-          <div className="bio p-3 text-[18px] w-[80%] text-justify leading-loose">
+          <div className="bio p-3 w-[95%] text-justify leading-loose">
             <p className="mb-4">I am a Frontend Developer with about three years of experience in using HTML, CSS and vanilla JS along with React, Next.js, Vite and Tailwind frameworks. As a current ALX Software Engineering student and an avid learner who is passionate about technological innovations, I enjoy partnering with others to work on exciting projects.</p>
-            <p className={`${showMore ? "block" : "hidden"}`}>Before going into frontend development, i worked as a graphics designer for an organisation that helps to create awareness about climate change. With my knowledge of graphics design, and zeal to improve and advance my skills, i decided to look into frontend development in 2021, i took courses on udemy and also enrolled with Zuri where i was trained on the use of basic HTML, CSS and JS. I recently had the pleasure of working with Adepoju Tobi on two projects, a Patricia website clone and a Netflix website clone. In the Patricia clone, I worked on the about page and sign-up page, while Tobi worked on the landing page and register page. For the Netflix clone, i focused on the sign-up and registration pages, while Tobi worked on the movie page.</p>
+            <p className={`${showMore ? "block" : "hidden"}`}>Before venturing into frontend development, i worked as a graphics designer for an organisation that helps to create awareness about climate change. With my knowledge of graphics design, and zeal to improve and advance my skills, i decided to look into frontend development in 2021, i took courses on udemy and also enrolled with Zuri where i was trained on the use of basic HTML, CSS and JS. I recently had the pleasure of working with Adepoju Tobi on two projects, a Patricia website clone and a Netflix website clone. In the Patricia clone, I worked on the about page and sign-up page, while Tobi worked on the landing page and register page. For the Netflix clone, i focused on the sign-up and registration pages, while Tobi worked on the movie page.</p>
           </div>
         </div>
         <div className="flex justify-center">
@@ -67,32 +67,32 @@ function Portfolio () {
       <div>
         <div className="mt-[8rem]">
           <div className="flex justify-center">
-            <p className="headings text-[30px] text-neutral-600 font-bold text-center">LANGUAGES</p>
+            <p className="headings text-neutral-600 font-bold text-center">LANGUAGES</p>
           </div>
           <div className="flex justify-evenly mt-[5rem]">
-            <img src={Html} alt="HTML" className="lang html w-[15%]"></img>
-            <img src={Css} alt="CSS" className="lang w-[10%]"></img>
-            <img src={Js} alt="JS" className="lang w-[10%]"></img>
+            <img src={Html} alt="HTML" className="html"></img>
+            <img src={Css} alt="CSS" className="lang"></img>
+            <img src={Js} alt="JS" className="lang"></img>
           </div>
         </div>
         <div className="mt-[6rem]">
           <div className="flex justify-center">
-            <p className="headings text-[30px] text-neutral-600 font-bold text-center">FRAMEWORKS</p>
+            <p className="headings text-neutral-600 font-bold text-center">FRAMEWORKS</p>
           </div>
           <div className="flex justify-evenly mt-[5rem]">
-            <div className="icon w-[10%]">
+            <div className="icon">
               <img src={ReactIcon} alt="React"></img>
             </div>
-            <img src={Vite} alt="Vite" className="icon w-[10%]"></img>
-            <div className="icon vary w-[20%]">
+            <img src={Vite} alt="Vite" className="icon"></img>
+            <div className="vary">
               <img src={Next} alt="Next"></img>
             </div>
-            <img src={Tail} alt="Tailwind" className="icon w-[10%]"></img>
+            <img src={Tail} alt="Tailwind" className="icon"></img>
           </div>
         </div>
         <div className="mt-[6rem]">
           <div className="flex justify-center">
-            <p className="headings text-[30px] text-neutral-600 font-bold text-center">PROJECTS</p>
+            <p className="headings text-neutral-600 font-bold text-center">PROJECTS</p>
           </div>
           <div className="image-main-container flex justify-start mt-[5rem] p-4 gap-x-8 ">
             <div className="image-container">
@@ -103,21 +103,21 @@ function Portfolio () {
             </div>
           </div>
           <div className="view-container flex justify-center">
-            <button className="view w-[25%] h-[50px] rounded-[40px] text-white font-bold mt-[3rem] bg-[#8c53bb] hover:bg-[#861edb] relative top-[-300px] left-[350px] shadow-lg"><a href="https://patricia-clone.vercel.app">View page</a></button>
+            <button className="view w-[25%] h-[50px] rounded-[40px] text-white font-bold mt-[3rem] bg-[#8c53bb] hover:bg-[#861edb] shadow-lg"><a href="https://patricia-clone.vercel.app">View page</a></button>
           </div>
-          <div className="flex flex-wrap m-5">
-            <img src={Climate} alt="climate" className="project w-[25%] m-4 shadow-lg transition-transform transform hover:scale-110"></img>
-            <img src={Workshop} alt="workshop" className="project w-[25%] m-4 shadow-lg transition-transform transform hover:scale-110"></img>
-            <img src={Surprise} alt="surprise" className="project w-[25%] m-4 shadow-lg transition-transform transform hover:scale-110"></img>
-            <img src={Staytuned} alt="staytuned" className="project w-[25%] m-4 shadow-lg transition-transform transform hover:scale-110"></img>
-            <img src={Apply} alt="apply" className="project w-[25%] m-4 shadow-lg transition-transform transform hover:scale-110"></img>
-            <img src={Welcome} alt="welcome" className="project w-[25%] m-4 shadow-lg transition-transform transform hover:scale-110"></img>
-            <img src={Design} alt="design" className="project w-[25%] m-4 shadow-lg transition-transform transform hover:scale-110"></img>
+          <div className="project-container flex flex-wrap m-5">
+            <img src={Climate} alt="climate" className="project m-4 shadow-lg transition-transform transform hover:scale-110"></img>
+            <img src={Workshop} alt="workshop" className="project m-4 shadow-lg transition-transform transform hover:scale-110"></img>
+            <img src={Surprise} alt="surprise" className="project m-4 shadow-lg transition-transform transform hover:scale-110"></img>
+            <img src={Staytuned} alt="staytuned" className="project m-4 shadow-lg transition-transform transform hover:scale-110"></img>
+            <img src={Apply} alt="apply" className="project m-4 shadow-lg transition-transform transform hover:scale-110"></img>
+            <img src={Welcome} alt="welcome" className="project m-4 shadow-lg transition-transform transform hover:scale-110"></img>
+            <img src={Design} alt="design" className="project m-4 shadow-lg transition-transform transform hover:scale-110"></img>
           </div>
         </div>
         <div className="mt-[6rem]">
           <div className="flex justify-center">
-            <p className="headings text-[30px] text-neutral-600 font-bold text-center">CONNECT WITH ME: </p>
+            <p className="headings text-neutral-600 font-bold text-center">CONNECT WITH ME: </p>
           </div>
           <div className="mt-[5rem] flex justify-evenly">
             <div className="w-[10%]">
@@ -134,7 +134,7 @@ function Portfolio () {
             </div>
           </div>
         </div>
-        <div className="flex justify-center mt-[6rem] text-neutral-400 text-[20px]">Designed by Awoyemi Boluwatife</div>
+        <div className="flex justify-center mt-[6rem] text-neutral-400 text-[15px]">Designed by Awoyemi Boluwatife</div>
       </div>
     </div>
   )
